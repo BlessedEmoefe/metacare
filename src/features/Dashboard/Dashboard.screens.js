43c5df58029b1049e97b0c68edcaf86ff8c61fc1
blemo <blessedemoefe1122334455@gmail.com>
2022-04-17 23:59:44 +0100

    updating repo

--- src/features/Dashboard/Dashboard.screens.js
+++ src/features/Dashboard/Dashboard.screens.js
@@ -26,6 +26,11 @@ const Dashboard = () => {
         searchBar
         btnText={"Export"}
       />
+      <SubHeader
+        directory={"Efficiency Analytics"}
+        searchBar
+        btnText={"Export"}
+      />
       <AnalyticsSection>
         {ChartData.map((item) => (
           <>
